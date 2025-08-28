eca1e80e6825dec0fc86076cd0ea11a45d2adacc
Eason0628 <zhaowintoo@gmail.com>
2025-08-28 17:20:37 +0800

    4.Adding the configuration and proxy strategies.

--- packages/project/vite_vue/vite.config.js
+++ packages/project/vite_vue/vite.config.js
@@ -6,8 +6,37 @@ import path, { dirname } from "path";
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      "@common": path.resolve(__dirname, "../../common"),
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  server: {
+    port: 3000,
+    inline: false,
+    host: '0.0.0.0',
+    // host: '127.0.0.1',
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:1080",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      }
+    },
+  },
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    minify: "terser",
+    sourcemap: false,
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+  }
 })
